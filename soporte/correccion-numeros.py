2811f3b245fbcaa480f86bfa151c9cdaabf4231d
Tamgama <tamara.garmart@gmail.com>
2024-09-23 12:16:07 +0200

    [UPD] data.csv: fixing phone numbers

--- soporte/correccion-numeros.py
+++ soporte/correccion-numeros.py
@@ -11,7 +11,7 @@ headers = {
     "accept-encoding": "gzip, deflate, br, zstd",
     "accept-language": "es,en-US;q=0.9,en;q=0.8",
     "cache-control": "no-cache",
-    "cookie": "didomi_token=eyJ1c2VyX2lkIjoiMTkwYTY0NjAtNGU2Yy02NjAyLWIzYjctZjY5ZTMwODYyZWJmIiwiY3JlYXRlZCI6IjIwMjQtMDctMTJUMDk6Mjg6NDUuMDMwWiIsInVwZGF0ZWQiOiIyMDI0LTA3LTEyVDA5OjI4OjQ2LjI1MVoiLCJ2ZW5kb3JzIjp7ImVuYWJsZWQiOlsiZ29vZ2xlIiwiYzpsaW5rZWRpbi1tYXJrZXRpbmctc29sdXRpb25zIiwiYzptaXhwYW5lbCIsImM6YWJ0YXN0eS1MTGtFQ0NqOCIsImM6aG90amFyIiwiYzpiZWFtZXItSDd0cjdIaXgiLCJjOnRlYWxpdW1jby1EVkRDZDhaUCIsImM6dGlrdG9rLUtaQVVRTFo5IiwiYzpnb29nbGVhbmEtNFRYbkppZ1IiLCJjOmlkZWFsaXN0YS1MenRCZXFFMyIsImM6aWRlYWxpc3RhLWZlUkVqZTJjIiwiYzpjb250ZW50c3F1YXJlIiwiYzptaWNyb3NvZnQiXX0sInB1cnBvc2VzIjp7ImVuYWJsZWQiOlsiZ2VvbG9jYXRpb25fZGF0YSIsImRldmljZV9jaGFyYWN0ZXJpc3RpY3MiXX0sInZlcnNpb24iOjIsImFjIjoiQ2hHQUVBRmtGQ0lBLkFBQUEifQ==; euconsent-v2=CQBpHQAQBpHQAAHABBENA8EsAP_gAAAAAAAAHXwBwAIAAqABaAFsAUgC8wHXgAAAFJQAYAAgpWUgAwABBSshABgACClY6ADAAEFKwkAGAAIKVgAA.f_wAAAAAAAAA; userUUID=443c4bb3-401a-4cb8-a32e-d3db477f4ce6; SESSION=e87100649bd6e940~a2bf2ff4-88a9-4272-95dd-d24838ce6713; utag_main__sn=6; utag_main_ses_id=1724068701892%3Bexp-session; utag_main__prevVtUrl=https%3A%2F%2Fwww.idealista.com%2F%3Bexp-1724072301945; utag_main__prevVtUrlReferrer=%3Bexp-1724072301945; utag_main__prevVtSource=Direct traffic%3Bexp-1724072301945; utag_main__prevVtCampaignName=organicWeb%3Bexp-1724072301945; utag_main__prevVtCampaignCode=%3Bexp-1724072301945; utag_main__prevVtCampaignLinkName=%3Bexp-1724072301945; utag_main__prevVtRecipientId=%3Bexp-1724072301945; utag_main__prevVtProvider=%3Bexp-1724072301945; utag_main__ss=0%3Bexp-session; contacta2bf2ff4-88a9-4272-95dd-d24838ce6713=\"{'maxNumberContactsAllow':10}\"; cookieSearch-1=\"/venta-viviendas/murcia-murcia/:1724068708141\"; _last_search=officialZone; senda2bf2ff4-88a9-4272-95dd-d24838ce6713=\"{}\"; utag_main__pn=4%3Bexp-session; utag_main__prevCompletePageName=005-idealista/portal > portal > detail > openGallery%3Bexp-1724072746057; utag_main__prevLevel2=005-idealista/portal%3Bexp-1724072746057; utag_main__se=10%3Bexp-session; utag_main__st=1724070947956%3Bexp-session; utag_main__prevCompleteClickName=005-idealista/portal > portal > closeGallery; datadome=hyjMqJcB2mt_N8dXXo3pGz~c8~Zl6nK5UhLIYlHdru4CL7NEr5jxCTaMawj4WZBrBZXE66lZsMWjoxJnO5~QCAewHiFgD1CwrAZdkJr7Lsni_yo1hFynlwl5aAtxKBQm",
+    "cookie": "userUUID=f784004d-fd85-40c5-bfa0-3d2a9ad1c125; _last_search=officialZone; _pprv=eyJjb25zZW50Ijp7IjAiOnsibW9kZSI6Im9wdC1pbiJ9LCIxIjp7Im1vZGUiOiJvcHQtaW4ifSwiMiI6eyJtb2RlIjoib3B0LWluIn0sIjMiOnsibW9kZSI6Im9wdC1pbiJ9LCI0Ijp7Im1vZGUiOiJvcHQtaW4ifSwiNSI6eyJtb2RlIjoib3B0LWluIn0sIjYiOnsibW9kZSI6Im9wdC1pbiJ9LCI3Ijp7Im1vZGUiOiJvcHQtaW4ifX0sInB1cnBvc2VzIjpudWxsLCJfdCI6Im1oMzJva2g5fG0xZW5yMzU5In0%3D; __rtbh.uid=%7B%22eventType%22%3A%22uid%22%2C%22id%22%3A%22unknown%22%7D; __rtbh.lid=%7B%22eventType%22%3A%22lid%22%2C%22id%22%3A%22xUju80WjymLwBmCZIvDM%22%7D; _pcid=%7B%22browserId%22%3A%22m1enr357f2d4699a%22%2C%22_t%22%3A%22mh32ol01%7Cm1enr3o1%22%7D; _pctx=%7Bu%7DN4IgrgzgpgThIC4B2YA2qA05owMoBcBDfSREQpAeyRCwgEt8oBJAE0RXSwH18yBbABYBmAEyVUABmEAffgEYoSGMMrCQAXyA; didomi_token=eyJ1c2VyX2lkIjoiMTkyMWRhZTQtMzVlOS02YmUxLWI2NzYtYjA4ZDQwYWU0NzNjIiwiY3JlYXRlZCI6IjIwMjQtMDktMjNUMDc6MDA6MTIuNzY2WiIsInVwZGF0ZWQiOiIyMDI0LTA5LTIzVDA3OjQxOjU5Ljk0MFoiLCJ2ZW5kb3JzIjp7ImRpc2FibGVkIjpbImdvb2dsZSIsImM6bGlua2VkaW4tbWFya2V0aW5nLXNvbHV0aW9ucyIsImM6bWl4cGFuZWwiLCJjOmFidGFzdHktTExrRUNDajgiLCJjOmhvdGphciIsImM6YmVhbWVyLUg3dHI3SGl4IiwiYzp0ZWFsaXVtY28tRFZEQ2Q4WlAiLCJjOnRpa3Rvay1LWkFVUUxaOSIsImM6Z29vZ2xlYW5hLTRUWG5KaWdSIiwiYzppZGVhbGlzdGEtTHp0QmVxRTMiLCJjOmlkZWFsaXN0YS1mZVJFamUyYyIsImM6Y29udGVudHNxdWFyZSIsImM6bWljcm9zb2Z0Il19LCJwdXJwb3NlcyI6eyJkaXNhYmxlZCI6WyJnZW9sb2NhdGlvbl9kYXRhIiwiZGV2aWNlX2NoYXJhY3RlcmlzdGljcyJdfSwidmVyc2lvbiI6MiwiYWMiOiJBQUFBLkFBQUEifQ==; euconsent-v2=CQFZtsAQFZtsAAHABBENBHFgAAAAAAAAAAAAAAAAAACkoAMAAQU3KQAYAAgpuQgAwABBTcdABgACCm4SADAAEFNw.YAAAAAAAAAAA; utag_main__sn=2; contact95d1e63a-7d07-4e75-a334-5860d75d2119=\"{'maxNumberContactsAllow':10}\"; send95d1e63a-7d07-4e75-a334-5860d75d2119=\"{}\"; SESSION=1b732e08a8140e05~95d1e63a-7d07-4e75-a334-5860d75d2119; cookieSearch-1=\"/alquiler-viviendas/murcia/pedanias-norte/espinardo/:1727077339696\"; utag_main__prevCompleteClickName=005-idealista/portal > portal > seePhotos; datadome=i8IBIe6QTKjdIrIVOWXYP9ae0KawHupLGkXY8Ytg0PNAMBH~HzWmXOHtNbBgnj0lBkGKR47I_5gnqBL6fKBs3ZT6~QNb~4pUxsMmtPIrAZq8QR1vo_mzcRcc8bNDLd2m",
     "pragma": "no-cache",
     "priority": "u=0, i",
     "sec-ch-device-memory": "8",
@@ -30,33 +30,46 @@ headers = {
 }
 
 
+# Crear sesión
 session = requests.Session()
 session.headers.update(headers)
-# Leer el archivo Excel
-df = pd.read_excel("inmuebles.xlsx")
+
+# Leer el archivo CSV
+df = pd.read_csv("scraping/src/data.csv")
 
 # Definir la URL base para obtener el número de teléfono
 url_base = "https://www.idealista.com/es/ajax/ads/{}/contact-phones"
-print(url_base)
+
 # Iterar sobre las filas del DataFrame
 for index, row in df.iterrows():
-    if pd.isna(row['Teléfono']) or row['Teléfono'] == 'N/A':
-        id_inmueble = int(row['ID Inmueble'])
+    # Verificar si falta el número de teléfono (vacío o N/A)
+    if pd.isna(row['tlf']) or row['tlf'] == 'N/A':
+        id_inmueble = int(row['id'])
         url = url_base.format(id_inmueble)   
-        # Hacer una solicitud a la URL para obtener el número de teléfono
-        print(url)
+        
+        # Hacer la solicitud para obtener el número de teléfono
+        print(f"Procesando ID: {id_inmueble} - URL: {url}")
         response = session.get(url)
-        time.sleep(random.uniform(1, 5))  # Añadir un retraso
+        
+        # Introducir un retraso aleatorio entre solicitudes para evitar bloqueos
+        time.sleep(random.uniform(1, 5))  
+        
         if response.status_code == 200:
-            data = response.json()
-            print (data)
-            if 'phone1' in data and data['phone1'] and 'number' in data['phone1']:
-                phone_number = data['phone1']['number']
-                df.at[index, 'Teléfono'] = phone_number
-                df.to_excel('numeros-bien.xlsx', index=False)
-            else:
-                print(f"No se encontró el número de teléfono para el ID de inmueble: {id_inmueble}")
+            try:
+                # Intentar obtener y parsear los datos JSON
+                data = response.json()
+                # Verificar si los datos del teléfono están presentes
+                if 'phone1' in data and data['phone1'] and 'number' in data['phone1']:
+                    phone_number = data['phone1']['number']
+                    df.at[index, 'tlf'] = phone_number  # Actualizar en el DataFrame
+                    print(f"Teléfono {phone_number} asignado para el ID de inmueble: {id_inmueble}")
+                else:
+                    print(f"No se encontró el número de teléfono para el ID de inmueble: {id_inmueble}")
+            except ValueError:
+                print(f"Error al parsear la respuesta JSON para el ID de inmueble: {id_inmueble}")
         else:
             print(f"Error {response.status_code} al obtener los datos para el ID de inmueble: {id_inmueble}")
-# Guardar el archivo Excel actualizado
-df.to_excel('numeros-bien.xlsx', index=False)
+
+# Guardar el archivo CSV actualizado solo una vez después de procesar todos los inmuebles
+df.to_csv('scraping/src/data.csv', index=False)
+print("Archivo CSV actualizado guardado correctamente.")
